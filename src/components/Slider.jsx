d240ade29efe2745efdecf187b7a5a3e19d223f6
chouella <kanyonga.chouell@gmail.com>
2021-11-09 12:59:22 +0300

    Sliderchange

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import{ArrowLeftOutlined, ArrowRightOutlined} from "@material-ui/icons"
 import styled from 'styled-components'
+import {sliderItems} from "../data"
+import React, { useEffect, useState } from "react";
 
 const Container = styled.div`
 width: 100%;
@@ -7,7 +9,7 @@ height: 100vh;
 display: flex;
 
 position: relative;
-
+overflow: hidden;
 `
 const Arrow =styled.div`
 width: 50px;
@@ -25,43 +27,96 @@ right: ${props=> props.direction === "right" && " 10px"};
 margin: auto;
 cursor: pointer;
 opacity: 0.5;
+z-index: 2;
 `;
 const Wrapper= styled.div`
 height: 100%;
+display:flex;
+transform: translateX(${(props)=> props.slideIndex * -100}vw);
 `;
 const  Slide =styled.div`
 width: 100vw;
 height: 100vh;
 display:flex;
 align-item: center;
+background-color: #${props=>props.bg}
+
 `;
 const  ImgContainer =styled.div`
 height: 100%;
-flex:1;
 
+flex:1;
+margin-left: 50px;
 `;
 const Image = styled.img`
-align-items: center;
-height:80%;
 
+height:100%;
+
+border-radius: 100%;
+top: 1px;
+right: 40%;
+margin: 10px;
 `;
 const  InfoContainer =styled.div`
 flex:1;
+padding: 60px;
+bottom: 2px;
+margin: 20px;
 `;
+const Title=styled.h1`
+font-size: 70px;
+margin: 50px 0px;
+`
+
+const Desc=styled.p`
+margin: 60px 0px;
+font-size: 20px;
+font-weight: 550;
+letter-spacing: 3px;
+`;
+const Button=styled.button`
+padding : 10px;
+font-size: 20px;
+background-color: transparent;
+cursor: pointer;
+`;
+
 const Slider = () => {
+    const[slideIndex, setSlideIndex]= useState(0);
+
+    const handleClick = (direction) => {
+
+if(direction==="left"){
+    setSlideIndex(slideIndex > 0? slideIndex-1 :2)
+}else{
+    setSlideIndex(slideIndex <2 ? slideIndex +1 :0)
+}
+    };
     return (
         <Container>
-            <Arrow direction="left">
+            <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlined/>
             </Arrow>
-            <Wrapper>
-            <Slide>
-<ImgContainer> 
-<Image  src="https://images.pexels.com/photos/6809021/pexels-photo-6809021.jpeg?cs=srgb&dl=pexels-philip-warp-6809021.jpg&fm=jpg"/>
-</ImgContainer>
-</Slide>
+            <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((item) => (
+                <Slide bg={item.bg}>
+                <ImgContainer> 
+                <Image  src={item.img}/>
+                </ImgContainer>
+                <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOP NOW</Button>
+                
+                </InfoContainer>
+                </Slide> 
+            ))}
+            
+
+
+
             </Wrapper>
-            <Arrow direction="right">
+            <Arrow direction="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlined/>
             </Arrow>
         </Container>
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems=[
+
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/10054914/pexels-photo-10054914.jpeg?cs=srgb&dl=pexels-ron-lach-10054914.jpg&fm=jpg",
+        title: "FALL SALE",
+        desc:"ALL WOMEN PRODUCT AVAILABLE ON GECHOU,WITH A 40% DISCOUNT ON EVERY PRODUCT",
+        bg:"C37B89",
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/6621174/pexels-photo-6621174.jpeg?cs=srgb&dl=pexels-monstera-6621174.jpg&fm=jpg",
+        title: "POPULAR SALE",
+        desc:"ALL WOMEN PRODUCT AVAILABLE ON GECHOU,WITH A 40% DISCOUNT ON EVERY PRODUCT",
+        bg:"BCCC9A",
+    },
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/8804995/pexels-photo-8804995.jpeg?cs=srgb&dl=pexels-mike-jones-8804995.jpg&fm=jpg",
+        title: "WINTER SALE",
+        desc:"ALL WOMEN PRODUCT AVAILABLE ON GECHOU,WITH A 40% DISCOUNT ON EVERY PRODUCT",
+        bg:"D1E8E4",
+    },
+]
